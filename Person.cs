d020963938fb8b91c9cdc53f48bad5f69e099f2c
shintanabilla23 <62046549+shintanabilla23@users.noreply.github.com>
2020-04-11 12:27:00 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The person has name: {0} and age: {1}", Name, Age);
+        }
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, int age, string studentid, string email) : base(name, age)
+        {
+            this.StudentId = studentid;
+            this.Email = email;
+        }
+
+    }
+}
